e5abab1f77626b4cc775376135710dad9f042fab
Orlando Challenger <orlandofat@gmail.com>
2020-05-08 16:50:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["mat", "sam", "tom", "ben"]
+  #array = ["mat", "sam", "tom", "ben"]
   array.shift
 end
 
