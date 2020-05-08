b567c15e127f8a56d9e9926c328d9657d0b2c025
Orlando Challenger <orlandofat@gmail.com>
2020-05-08 16:23:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["mat", "sam", "tom", "ben"]
-  "mat" = array.shift
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
